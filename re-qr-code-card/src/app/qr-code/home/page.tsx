2e4fb930c61a24b0ea6d7ae653fadebd3ea95c1e
ianer-glitch <ianrepker@gmail.com>
2025-06-07 13:37:41 -0300

    feat : added button to project

--- re-qr-code-card/src/app/qr-code/home/page.tsx
+++ re-qr-code-card/src/app/qr-code/home/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/shared/components/atoms/button/Button";
+import { Button } from "@/shared/components/atoms/button/button";
+import { Input } from "@/shared/components/atoms/input/input";
 import { Title } from "@/shared/components/atoms/title";
 
 export default function Home(){
     return (
         <div>
             <Title>Re Qr Code</Title>
-            <Button >Some Text</Button>
+            <Input label="Place the text to be converted"/>
+            <Button >Go !</Button>
         </div>
     )
 }
---
+++ re-qr-code-card/src/shared/components/atoms/input/input.tsx
@@ -0,0 +1,15 @@
+import { Input as Inp } from "@/components/ui/input"
+import { ComponentProps } from "react"
+
+export type InputProps = ComponentProps<typeof Inp> & {
+    label? : string
+}
+
+export const Input = ({label, ...props} : InputProps) =>{
+    return (
+        <fieldset>
+            <label>{label}</label>
+            <Inp {...props}/>
+        </fieldset>
+    )
+}
