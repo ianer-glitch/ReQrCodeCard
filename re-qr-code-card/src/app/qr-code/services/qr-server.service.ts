80d2a0abc688d0a2fe13668367eb0c0ddc50314b
ianer-glitch <ianrepker@gmail.com>
2025-06-14 07:46:47 -0300

    feat: configured env

--- re-qr-code-card/src/app/qr-code/services/qr-server.service.ts
+++ re-qr-code-card/src/app/qr-code/services/qr-server.service.ts
@@ -3,9 +3,10 @@ import { blobToBase64 } from "@/shared/utils/blobToBase64"
 
 export const createQrCode = async (size: string, data: string) => {
   const response = await api.get(
-    `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${data}`,
+    `/create-qr-code/?size=${size}x${size}&data=${data}`,
     { responseType: 'blob' }
   )
 
   return blobToBase64(response.data)
 }
+
--- re-qr-code-card/src/shared/config/axios.ts
+++ re-qr-code-card/src/shared/config/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-});
+  baseURL: process.env.NEXT_PUBLIC_QR_CODE_API_URL,
+} );
+  
